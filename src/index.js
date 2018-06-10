a5a0e484ba7dbe79b4a0928924196965d8703d3f
Anjali Pipalia <arp226@njit.edu>
2018-06-09 18:32:11 -0700

    Two step process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 /**
  * Created by arp226 on 6/9/2018.
  */
+//Create a new component. This component should produce some HTML
+
+
+
+// Take this component's generated HTML and put it on the page(in DOM)
