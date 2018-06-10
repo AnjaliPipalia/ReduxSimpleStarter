900dfd2b2d72ba074bf18687caa06072ab312bdd
Anjali Pipalia <arp226@njit.edu>
2018-06-09 23:56:20 -0700

    displayed first component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 /**
  * Created by arp226 on 6/9/2018.
  */
-//Create a new component. This component should produce some HTML
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Create a new component. This component should produce some HTML
+const App = function(){
+    return <div>Hi!!</div>
+}
 
 
 // Take this component's generated HTML and put it on the page(in DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
